2a3878882b26c8e1da2df160eb6eac8e40fde062
Nicholas Peters <thenootlinja@gmail.com>
2021-04-06 20:53:03 -0400

    Add image, product name, and product price to the product component

---
+++ sick-fits/frontend/components/Products.js
@@ -0,0 +1,46 @@
+import { useQuery } from '@apollo/client';
+import { id } from 'date-fns/locale';
+import gql from 'graphql-tag';
+import styled from 'styled-components';
+import Product from './Product';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
+const ProductsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
+export default function Products() {
+  // We do not need a callback function for the variables below as they are reactive
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
+  console.log(data, error, loading);
+  // This could be replaced with a loader animation
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>ERROR: {error.message}</p>;
+  return (
+    <div>
+      <ProductsListStyles>
+        {data.allProducts.map((product) => (
+          <Product key={product.id} product={product} />
+        ))}
+      </ProductsListStyles>
+    </div>
+  );
+}
--- sick-fits/frontend/pages/_app.js
+++ sick-fits/frontend/pages/_app.js
@@ -1,18 +1,39 @@
+import { ApolloProvider } from '@apollo/client';
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
 
 // TODO: Swap with our own styles
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+// Using the getInitialProps function which is async
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  // Creating an variable that is equal to an empty object at the start
+  let pageProps = {};
+  // Checking if the component that was passed in has initial props
+  if (Component.getInitialProps) {
+    // If it does, set pageProps to the value returned from Component.getInitialProps(ctx)
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  // setting the query property of the pageProps variable to ctx.query
+  pageProps.query = ctx.query;
+  // returning the destructured pageProps object
+  return { pageProps };
+};
+
+export default withData(MyApp);
