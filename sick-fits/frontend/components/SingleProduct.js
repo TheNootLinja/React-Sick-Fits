304a7655d5bfd36131eaab45a1557f0e53293dc2
TheNootLinja <thenootlinja@gmail.com>
2021-04-17 13:00:02 -0400

    Add Head and title tag to SingleProduct.js so we can control the page title, also add product information such as image, name and display to page

--- sick-fits/frontend/components/SingleProduct.js
+++ sick-fits/frontend/components/SingleProduct.js
@@ -1,6 +1,22 @@
 import { useQuery } from "@apollo/client"
 import gql from "graphql-tag";
 import DisplayError from "./ErrorMessage";
+import Head from 'next/head';
+import styled from "styled-components";
+
+const ProductStyles = styled.div`
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-auto-flow: column;
+    max-width: var(--maxWidth);
+    justify-content: center;
+    align-items: top;
+    gap: 2rem;
+    img {
+        width: 100%;
+        object-fit: contain;
+    }
+`;
 
 const SINGLE_ITEM_QUERY = gql`
 query SINGLE_ITEM_QUERY($id: ID!){
@@ -10,6 +26,13 @@ query SINGLE_ITEM_QUERY($id: ID!){
     name
     price
     description
+    id
+    photo {
+        altText
+        image {
+            publicUrlTransformed
+        }
+    }
   }
 }
 `;
@@ -23,7 +46,15 @@ export default function SingleProduct({ id }) {
     console.log(data)
     if(loading) return <p>Loading...</p>
     if(error) return <DisplayError error={error} />
-    return <div>
-        <h2>{data.Product.name}</h2>
-    </div>
+    const { Product } = data;
+    return <ProductStyles>
+        <Head>
+            <title>Sick Fits | {Product.name}</title>
+        </Head>
+        <img src={Product.photo.image.publicUrlTransformed} alt={Product.photo.altText}/>
+        <div className="details">
+            <h2>{Product.name}</h2>
+            <p>{Product.description}</p>
+        </div>
+    </ProductStyles>
 }
